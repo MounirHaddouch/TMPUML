b3302639b41d65787b98ce2d5903c5af0ae9ba71
MounirHaddouch <haddouch-mounir@hotmail.com>
2014-06-13 10:30:31 -0300

    Premier commit "MounirHaddouch"

---
+++ src/tpuml/SimpleConversion.java
@@ -0,0 +1,13 @@
+package tpuml;
+
+public class SimpleConversion extends Conversion {
+
+	public SimpleConversion(Unit source, Unit target, double amount) {
+		super(source, target, amount);
+	}
+	
+	public double SimpleConv(){
+		return ((getSource().getFactor()*getAmount())/getTarget().getFactor());
+	}
+}
+
